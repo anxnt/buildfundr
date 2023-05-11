7c6874634b9564889ca2eb5cc3867b5d61deb7e1
Anant <anant7.ak@gmail.com>
2023-05-11 17:42:56 +0530

    Update Secondhead.js

--- pages/Components/Secondhead.js
+++ pages/Components/Secondhead.js
@@ -1,15 +1,16 @@
+import styles from '@/styles/Home.module.css'
 
 function Secondhead(props) {
   return (
-    <div style={{display:"flex", justifyContent:"space-between", alignItems:"center", height:"150px", width:"100vw",  fontFamily:"Manrope", fontWeight:"light"}}>
+    <div className={styles.theheader} style={{display:"flex", justifyContent:"space-between", alignItems:"center", height:"150px", width:"100vw",  fontFamily:"Manrope", fontWeight:"light"}}>
     <div style={{height:"50px", width:"400px", display:"flex", justifyContent:"center", alignItems:"center", color:"#DEDEDE", fontSize:"25px", }}>
     <a href="/"><h1> buildfundr</h1></a>
 
     </div>
 
     <div style={{height:"50px", width:"450px", display:"flex", justifyContent:"space-evenly", alignItems:"center", }}>
-    <h1 style={{color:"#5F5F5F", fontWeight:"lighter",fontSize:"35px"}}> about</h1>
-    <h1 style={{color:"#EBD070", fontWeight:"lighter",fontSize:"35px"}}> {props.name} </h1>
+    <h1 className={styles.menu} style={{color:"#5F5F5F", fontWeight:"lighter",fontSize:"35px"}}> about</h1>
+    <h1 className={styles.menu} style={{color:"#EBD070", fontWeight:"lighter",fontSize:"35px"}}> {props.name} </h1>
 
     </div>
     </div>
